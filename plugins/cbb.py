2fa36a534711236f5314bdf3fd899da519436815
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-12-15 09:26:11 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,5 +1,3 @@
-#(©)Codexbotz
-
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
@@ -10,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/rakeshyt/DevilFilesStore'>Click here</a>\n○ Channel : @RYMOFFICIAL\n○ Support Group : @JAIHINDCHATTING</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
